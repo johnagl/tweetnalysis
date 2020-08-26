1cc6e95bb685d5e81534acc1adc8bff5f5876dd5
John Aguilar <42220961+johnagl@users.noreply.github.com>
2020-08-25 18:49:08 -0700

    Test twitter api

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import request from 'request';
 
 dotenv.config();
 const app = express(); 
 
 const token = process.env.BEARER_TOKEN;
+const endpoint = 'https://api.twitter.com/2/tweets/search/recent?query=from:TwitterDev';
+
+request({ url: endpoint, json: true, headers : { 'Authorization' : 'Bearer ' + token}}, (error, response) => {
+    if (error) {
+        console.log(error);
+    } else if (response.body.data.length == 0) {
+        console.log('No tweets found for the following user');
+    } else {
+        console.log(response.body);
+    }
+});
+
 
 app.get('/', (req, res) => {
     res.send('Server');
