169ac5d957b5620215f1b6f3c584933bbae004ae
John Aguilar <42220961+johnagl@users.noreply.github.com>
2020-08-26 22:37:18 -0700

    Test NLP libraries

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import request from 'request';
+import Sentiment from 'sentiment';
+
+var Analyzer = require('natural').SentimentAnalyzer;
+var stemmer = require('natural').PorterStemmer;
+var analyzer = new Analyzer("English", stemmer, "afinn");
+// getSentiment expects an array of strings
+console.log(analyzer.getSentiment(["See", "how", "@PennMedCDH", "are", "using", "Twitter", "data", "to", "understand", "the", "COVID-19", "health", "crisis"]));
+
+const sentimentAnalyzer : Sentiment = new Sentiment();
+const result : Sentiment.AnalysisResult = sentimentAnalyzer.analyze('See how @PennMedCDH are using Twitter data to understand the COVID-19 health crisis');
+console.log(result);
 
 dotenv.config();
 const app = express(); 
@@ -14,7 +25,10 @@ request({ url: endpoint, json: true, headers : { 'Authorization' : 'Bearer ' + t
     } else if (response.body.data.length == 0) {
         console.log('No tweets found for the following user');
     } else {
-        console.log(response.body);
+        response.body.data.forEach((tweet : any) => {
+            console.log(tweet.text);
+            console.log(sentimentAnalyzer.analyze(tweet.text));
+        })
     }
 });
 
